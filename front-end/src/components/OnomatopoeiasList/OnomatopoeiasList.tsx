8c2af6c5e079a0cdbbaa3c6ed9c14877cfe88ad6
Cheryl Lee <clee0937@gmail.com>
2024-05-19 18:19:59 +0100

    created component for OnomatopoeiasList

---
+++ front-end/src/components/OnomatopoeiasList/OnomatopoeiasList.tsx
@@ -0,0 +1,28 @@
+import "./OnomatopoeiasList.scss";
+import Onomatopoeia from "../Onomatopoeia/Onomatopoeia";
+import OnomatopoeiaResponse from "../../types/OnomatopoeiaResponse";
+import { Link } from "react-router-dom";
+
+type OnomatopoeiasListProps = {
+  onomatopoeias: OnomatopoeiaResponse[];
+};
+
+const OnomatopoeiasList = ({ onomatopoeias }: OnomatopoeiasListProps) => {
+  return (
+    <>
+      <div className="onomatopoeia-list">
+        {onomatopoeias.map((onomatopoeia) => (
+          <Link
+            key={onomatopoeia.id}
+            to={""}
+            style={{ textDecoration: "none" }}
+          >
+            <Onomatopoeia onomatopoeia={onomatopoeia} />
+          </Link>
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default OnomatopoeiasList;
